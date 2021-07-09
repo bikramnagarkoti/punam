777f7d937b40e334d0ac06a88519cdf37e5af077
bikramnagarkoti <67859920+bikramnagarkoti@users.noreply.github.com>
2021-07-09 08:37:21 +0545

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,19 +94,19 @@ modalCloses.forEach((modalClose) =>{
     })
 })
 /*==================== PORTFOLIO SWIPER  ====================*/
-// let swiperPortfolio = new Swiper(".portfolio__container", {
-//     cssMode: true,
-//     loop:true,
+let swiperPortfolio = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop:true,
 
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable:true,
-//     },
-//   });
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+  });
 
 /*==================== TESTIMONIAL ====================*/
 let swiperTestimonial = new Swiper(".testimonial__container", {
